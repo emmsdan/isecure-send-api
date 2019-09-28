265c0700a9771b8529d73253943050ac28f889f5
Emmanuel Daniel <ecomje@gmail.com>
2019-09-28 17:55:00 +0100

    removed unused table columns

--- src/model/tables.sql
+++ src/model/tables.sql
@@ -9,8 +9,7 @@ CREATE TABLE IF NOT EXISTS `iss_files` (
   `expires_in` ENUM("5m", "1h", "12h", "1d", "3d", "5d", "7d") NOT NULL,
   `downloads_allowed` ENUM("1", "10", "15", "20", "25", "50", "100") NOT NULL,
   `file_url` varchar(255) NOT NULL,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `iss_file_url` (
@@ -18,7 +17,6 @@ CREATE TABLE  `iss_file_url` (
   `file_id` BINARY(38) NOT NULL,
   `downloads` int(11) DEFAULT 0 NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT joint_box
   FOREIGN KEY (file_id)
       REFERENCES iss_files(id)
